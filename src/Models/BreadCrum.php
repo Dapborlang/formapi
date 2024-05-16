08b81e1557ac7591fc2450f7e869aec3481481ee
Dapbor <dapbor88@gmail.com>
2024-05-16 20:09:14 +0530

    added breadcrum

---
+++ src/Models/BreadCrum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rdmarwein\FormApi\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BreadCrum extends Model
+{
+    use HasFactory;
+}
